995272b50d77f150298de1562694162dd136b779
Joe Lekie <joe.lekie@umbrexus.com>
2023-03-19 21:05:08 -0500

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/type';
+
+import { FluentTypeBuilder } from './lib/type';
+export const FluentType = new FluentTypeBuilder();
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,75 +1,84 @@
 import _ from 'lodash';
 
-import { Type, TSchema, TString, TNumber, TInteger, TBoolean, TObject, TPartial, TOptional, TAny, TNull, TUndefined, TNever, TVoid, TUnknown, TLiteral, TLiteralValue, TProperties, TArray, TUnion, TPromise, TFunction, StringOptions, NumericOptions, ObjectOptions, SchemaOptions, ArrayOptions, Static } from '@sinclair/typebox';
+import {
+    Type, Static,
+    TSchema, TString, TNumber, TInteger, TBoolean, TObject, TPartial, TOptional, TAny, TNull, TUndefined, TNever, TVoid, TUnknown, TLiteral, TLiteralValue, TProperties, TArray, TUnion, TPromise, TFunction, TPick, TOmit, TRecursive, TSelf,
+    StringOptions, NumericOptions, ObjectOptions, SchemaOptions, ArrayOptions
+} from '@sinclair/typebox';
 import { TypeCompiler, ValueError, TypeCheck } from '@sinclair/typebox/compiler';
 
-export interface WrappedObject {
-    [key: string]: WrapperBase<TSchema>;
+export interface FluentTypeObject {
+    [key: string]: FluentTypeBuilderBase<TSchema>;
 }
-export type MappedWrappedObjectProperties<T extends WrappedObject> = {
+export type MappedFluentTypeObjectProperties<T extends FluentTypeObject> = {
     [P in keyof T]: ReturnType<T[P]['schema']>;
 }
-export type MappedWrappedTuple<T extends [...any[]]> = {
+export type MappedFluentTypeTuple<T extends [...any[]]> = {
     [P in keyof T]: ReturnType<T[P]['schema']>;
 }
-export type MappedWrapped<T extends WrapperBase<TSchema>> = ReturnType<T['schema']>
+export type MappedWrapped<T extends FluentTypeBuilderBase<TSchema>> = ReturnType<T['schema']>
 
-export class RefactoredType {
+export class FluentTypeBuilder {
     public string(options?: StringOptions<string>) {
-        return new StringWrapper(Type.String(options));
+        return new StringFluentTypeBuilder(Type.String(options));
     }
     public number(options?: NumericOptions) {
-        return new NumberWrapper(Type.Number(options));
+        return new NumberFluentTypeBuilder(Type.Number(options));
     }
     public integer(options?: NumericOptions) {
-        return new IntegerWrapper(Type.Integer(options));
+        return new IntegerFluentTypeBuilder(Type.Integer(options));
     }
     public boolean(options?: SchemaOptions) {
-        return new BooleanWrapper(Type.Boolean(options));
+        return new BooleanFluentTypeBuilder(Type.Boolean(options));
     }
     public literal<T extends TLiteralValue>(value: T, options?: SchemaOptions) {
-        return new LiteralWrapper(Type.Literal(value, options));
+        return new LiteralFluentTypeBuilder(Type.Literal(value, options));
     }
     public any(options?: SchemaOptions) {
-        return new AnyWrapper(Type.Any(options));
+        return new AnyFluentTypeBuilder(Type.Any(options));
     }
     public null(options?: SchemaOptions) {
-        return new NullWrapper(Type.Null(options));
+        return new NullFluentTypeBuilder(Type.Null(options));
     }
     public undefined(options?: SchemaOptions) {
-        return new UndefinedWrapper(Type.Undefined(options));
+        return new UndefinedFluentTypeBuilder(Type.Undefined(options));
     }
     public never(options?: SchemaOptions) {
-        return new NeverWrapper(Type.Never(options));
+        return new NeverFluentTypeBuilder(Type.Never(options));
     }
     public void(options?: SchemaOptions) {
-        return new VoidWrapper(Type.Void(options));
+        return new VoidFluentTypeBuilder(Type.Void(options));
     }
     public unknown(options?: SchemaOptions) {
-        return new UnknownWrapper(Type.Unknown(options));
+        return new UnknownFluentTypeBuilder(Type.Unknown(options));
     }
-    public union<T extends WrapperBase<TSchema>[]>(schemas: [...T], options?: SchemaOptions) {
-        return new UnionWrapper(Type.Union([...schemas.map(s => s.schema()) as MappedWrappedTuple<T>], options));
+    public union<T extends FluentTypeBuilderBase<TSchema>[]>(schemas: [...T], options?: SchemaOptions) {
+        return new UnionFluentTypeBuilder(Type.Union([...schemas.map(s => s.schema()) as MappedFluentTypeTuple<T>], options));
     }
-    public object<T extends WrappedObject>(properties: T, options?: ObjectOptions) {
+    public object<T extends FluentTypeObject>(properties: T, options?: ObjectOptions) {
         const transformedProperties = _.transform(properties, (result, value, key) => {
             (result as any)[key] = value.schema();
-        }, {} as MappedWrappedObjectProperties<T>);
+        }, {} as MappedFluentTypeObjectProperties<T>);
 
-        return new ObjectWrapper(Type.Object(transformedProperties, options), properties);
+        return new ObjectFluentTypeBuilder(Type.Object(transformedProperties, options), properties);
     }
-    public array<T extends WrapperBase<TSchema>>(schema: T, options?: ArrayOptions) {
-        return new ArrayWrapper(Type.Array(schema.schema(), options)) as (T extends WrapperBase<infer TItem> ? ArrayWrapper<TItem> : ArrayWrapper<TSchema>);
+    public array<T extends FluentTypeBuilderBase<TSchema>>(schema: T, options?: ArrayOptions) {
+        return new ArrayFluentTypeBuilder(Type.Array(schema.schema(), options)) as (T extends FluentTypeBuilderBase<infer TItem> ? ArrayFluentTypeBuilder<TItem> : ArrayFluentTypeBuilder<TSchema>);
     }
-    public function<T extends WrapperBase<TSchema>[], U extends WrapperBase<TSchema>>(parameters: [...T], returns: U) {
-        return new FunctionWrapper(Type.Function([...parameters.map(p => p.schema()) as MappedWrappedTuple<T>], returns.schema())) as (U extends WrapperBase<infer TItem> ? FunctionWrapper<MappedWrappedTuple<T>, TItem> : FunctionWrapper<MappedWrappedTuple<T>, TSchema>);
+    public function<T extends FluentTypeBuilderBase<TSchema>[], U extends FluentTypeBuilderBase<TSchema>>(parameters: [...T], returns: U) {
+        return new FunctionFluentTypeBuilder(Type.Function([...parameters.map(p => p.schema()) as MappedFluentTypeTuple<T>], returns.schema())) as (U extends FluentTypeBuilderBase<infer TItem> ? FunctionFluentTypeBuilder<MappedFluentTypeTuple<T>, TItem> : FunctionFluentTypeBuilder<MappedFluentTypeTuple<T>, TSchema>);
+    }
+    public recursive<T extends TSchema>(callback: (self: SelfFluentTypeBuilder) => FluentTypeBuilderBase<T>, options?: SchemaOptions) {
+        return new RecursiveFluentTypeBuilder(Type.Recursive((self) => {
+            return callback(new SelfFluentTypeBuilder(self)).schema();
+        }, options));
     }
     public type<T extends TSchema>(schema: T) {
-        return new CustomWrapper(schema);
+        return new CustomFluentTypeBuilder(schema);
     }
 }
 
-export abstract class WrapperBase<T extends TSchema> {
+export abstract class FluentTypeBuilderBase<T extends TSchema> {
     protected type: T;
 
     public constructor(type: T) {
@@ -77,50 +86,50 @@ export abstract class WrapperBase<T extends TSchema> {
     }
 
     public optional() {
-        return new OptionalWrapper(Type.Optional(this.type));
+        return new OptionalFluentTypeBuilder(Type.Optional(this.type));
     }
     public nullable(options?: SchemaOptions) {
-        return new NullableWrapper(Type.Union([ Type.Null(), this.type ], options));
+        return new NullableFluentTypeBuilder(Type.Union([ Type.Null(), this.type ], options));
     }
     public array(options?: ArrayOptions) {
-        return new ArrayWrapper(Type.Array(this.type, options));
+        return new ArrayFluentTypeBuilder(Type.Array(this.type, options));
     }
     public promised(options?: SchemaOptions) {
-        return new PromiseWrapper(Type.Promise(this.type, options))
+        return new PromiseFluentTypeBuilder(Type.Promise(this.type, options))
     }
 
     public schema() {
         return this.type;
     }
     public compile() {
-        return new CompiledWrapper(TypeCompiler.Compile(this.type));
+        return new FluentTypeCheck(TypeCompiler.Compile(this.type));
     }
 }
-export class StringWrapper<T extends TString> extends WrapperBase<T> {
+export class StringFluentTypeBuilder<T extends TString> extends FluentTypeBuilderBase<T> {
 }
-export class NumberWrapper extends WrapperBase<TNumber> {
+export class NumberFluentTypeBuilder extends FluentTypeBuilderBase<TNumber> {
 }
-export class IntegerWrapper extends WrapperBase<TInteger> {
+export class IntegerFluentTypeBuilder extends FluentTypeBuilderBase<TInteger> {
 }
-export class BooleanWrapper extends WrapperBase<TBoolean> {
+export class BooleanFluentTypeBuilder extends FluentTypeBuilderBase<TBoolean> {
 }
-export class LiteralWrapper<T extends TLiteralValue> extends WrapperBase<TLiteral<T>> {
+export class LiteralFluentTypeBuilder<T extends TLiteralValue> extends FluentTypeBuilderBase<TLiteral<T>> {
 }
-export class AnyWrapper extends WrapperBase<TAny> {
+export class AnyFluentTypeBuilder extends FluentTypeBuilderBase<TAny> {
 }
-export class NullWrapper extends WrapperBase<TNull> {
+export class NullFluentTypeBuilder extends FluentTypeBuilderBase<TNull> {
 }
-export class UndefinedWrapper extends WrapperBase<TUndefined> {
+export class UndefinedFluentTypeBuilder extends FluentTypeBuilderBase<TUndefined> {
 }
-export class NeverWrapper extends WrapperBase<TNever> {
+export class NeverFluentTypeBuilder extends FluentTypeBuilderBase<TNever> {
 }
-export class VoidWrapper extends WrapperBase<TVoid> {
+export class VoidFluentTypeBuilder extends FluentTypeBuilderBase<TVoid> {
 }
-export class UnknownWrapper extends WrapperBase<TUnknown> {
+export class UnknownFluentTypeBuilder extends FluentTypeBuilderBase<TUnknown> {
 }
-export class UnionWrapper<T extends TSchema[]> extends WrapperBase<TUnion<T>> {
+export class UnionFluentTypeBuilder<T extends TSchema[]> extends FluentTypeBuilderBase<TUnion<T>> {
 }
-export class ObjectWrapper<T extends TObject, P extends WrappedObject> extends WrapperBase<T> {
+export class ObjectFluentTypeBuilder<T extends TObject, P extends FluentTypeObject> extends FluentTypeBuilderBase<T> {
     private properties: P;
 
     public constructor(schema: T, properties: P) {
@@ -129,33 +138,61 @@ export class ObjectWrapper<T extends TObject, P extends WrappedObject> extends W
         this.properties = properties;
     }
 
-    public partial() {
-        return new PartialWrapper(Type.Partial(this.type));
+    public partial(options?: ObjectOptions) {
+        return new PartialFluentTypeBuilder(Type.Partial(this.type, options));
     }
-    public extend<T extends WrappedObject>(properties: T) {
+    public extend<T extends FluentTypeObject>(properties: T, options?: SchemaOptions) {
         const transformedProperties = _.transform({ ...this.properties, ...properties }, (result, value, key) => {
             (result as any)[key] = value.schema();
-        }, {} as MappedWrappedObjectProperties<P & T>);
+        }, {} as MappedFluentTypeObjectProperties<P & T>);
 
-        return new ObjectWrapper(Type.Object(transformedProperties), properties)
+        return new ObjectFluentTypeBuilder(Type.Object(transformedProperties, options), properties);
     }
+
+    public pick<K extends keyof P>(keys: K, options?: SchemaOptions) {
+        const pickedProperties = _.pick(this.properties, keys);
+        const transformedProperties = _.transform(pickedProperties, (result, value: FluentTypeBuilderBase<TSchema>, key) => {
+            (result as any)[key] = value.schema();
+        }, {} as MappedFluentTypeObjectProperties<typeof pickedProperties>);
+
+        return new ObjectFluentTypeBuilder(Type.Object(transformedProperties, options), pickedProperties);
+    }
+    public omit<K extends keyof P>(keys: K, options?: SchemaOptions) {
+        const pickedProperties = _.omit(this.properties, keys);
+        const transformedProperties = _.transform(pickedProperties, (result, value: FluentTypeBuilderBase<TSchema>, key) => {
+            (result as any)[key] = value.schema();
+        }, {} as MappedFluentTypeObjectProperties<typeof pickedProperties>);
+
+        return new ObjectFluentTypeBuilder(Type.Object(transformedProperties, options), pickedProperties);
+    }
+
+    // public pick<K extends keyof P>(keys: (keyof T & string)[], options?: SchemaOptions) {
+    //     // const t = keys.map(k => Type.Literal(k));
+    //     const tmp = Type.Pick(this.schema(), Type.Union([ Type.Literal('test') ]), options);
+
+    //     return new PickFluentTypeBuilder(tmp);
+    // }
 }
-export class PartialWrapper<T extends TObject> extends WrapperBase<TPartial<T>> {
+export class PartialFluentTypeBuilder<T extends TObject> extends FluentTypeBuilderBase<TPartial<T>> {
 }
-export class OptionalWrapper<T extends TSchema> extends WrapperBase<TOptional<T>> {
+export class OptionalFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<TOptional<T>> {
 }
-export class NullableWrapper<T extends TSchema> extends WrapperBase<TUnion<[ TNull, T ]>> {
+export class NullableFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<TUnion<[ TNull, T ]>> {
 }
-export class ArrayWrapper<T extends TSchema> extends WrapperBase<TArray<T>> {
+export class ArrayFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<TArray<T>> {
 }
-export class PromiseWrapper<T extends TSchema> extends WrapperBase<TPromise<T>> {
+export class PromiseFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<TPromise<T>> {
 }
-export class FunctionWrapper<T extends readonly TSchema[], U extends TSchema> extends WrapperBase<TFunction<T, U>> {
+export class FunctionFluentTypeBuilder<T extends readonly TSchema[], U extends TSchema> extends FluentTypeBuilderBase<TFunction<T, U>> {
 }
-export class CustomWrapper<T extends TSchema> extends WrapperBase<T> {
+export class RecursiveFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<TRecursive<T>> {
+}
+export class SelfFluentTypeBuilder extends FluentTypeBuilderBase<TSelf> {
+}
+export class CustomFluentTypeBuilder<T extends TSchema> extends FluentTypeBuilderBase<T> {
 }
 
-export class CompiledWrapper<T extends TSchema> {
+export class FluentTypeCheck<T extends TSchema> {
     private typeCheck: TypeCheck<T>;
 
     public constructor(typeCheck: TypeCheck<T>) {
@@ -166,32 +203,3 @@ export class CompiledWrapper<T extends TSchema> {
         return this.typeCheck.Check(value);
     }
 }
-
-const RT = new RefactoredType();
-
-const Tmp1 = RT.object({
-    name: RT.string(),
-    values: RT.string().array().optional(),
-    test: RT.number().nullable(),
-    test2: RT.union([ RT.string(), RT.number(), RT.boolean() ]),
-    test3: RT.literal('abc'),
-    test4: RT.array(RT.string().nullable())
-});
-const Tmp2 = Tmp1.extend({
-    test5: RT.string(),
-    test6: RT.string().promised(),
-    test7: RT.function([ RT.string(), RT.boolean().optional() ], RT.number())
-})
-
-const Tmp3 = RT.function([ RT.string(), RT.boolean().optional() ], RT.number()).schema();
-
-const Tmp = Tmp2.compile();
-
-const test = {};
-if (!Tmp.check(test))
-    throw new Error();
-
-const aaa = test.name;
-test.test7('', false);
-// test.test4
-// console.log(aaa);
